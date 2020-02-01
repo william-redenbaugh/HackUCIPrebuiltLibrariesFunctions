00dda6e405dab5952ff5ee1f808ffca8e59b6145
William Redenbaugh <51929010+william-redenbaugh@users.noreply.github.com>
2020-02-01 03:52:37 -0800

    Update Heat_IOT.ino

--- Heaat_IOT/Heat_IOT.ino
+++ Heaat_IOT/Heat_IOT.ino
@@ -18,8 +18,8 @@ Fan fan;
 // Communication information
 #define AIO_SERVER      "io.adafruit.com"
 #define AIO_SERVERPORT  1883                  
-#define AIO_USERNAME    "wredenba"
-#define AIO_KEY         "aio_VOvt36DtLob6R6509gy6dP4VYMQV"
+#define AIO_USERNAME    "account_username"
+#define AIO_KEY         "account_password"
 
 // Objects to deal with actual connection and mqtt stuff. 
 WiFiClient client;
